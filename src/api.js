8372060b9d49f3cb016213c4d78a8197b1a5d0a9
Aamir Girach <khatriaamir202@gmail.com>
2025-08-16 10:19:52 +0530

    Added full code for weather app created using react

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'b532a5281fmsh9b9a20009a24122p1a8e16jsne0009ebc7fe0',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "855976620e59f6897037c3df77bc383d";
